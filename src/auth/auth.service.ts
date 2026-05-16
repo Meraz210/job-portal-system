61c2d673a74fb526607f5f4af6a015a46655a2dd
Meraz210 <merazahasan210@gmail.com>
2026-05-17 02:31:58 +0600

    Created user registration API

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+import { UsersService } from '../users/users.service';
+import { RegisterDto } from './dto/register.dto';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async register(registerDto: RegisterDto) {
+    return this.usersService.create(registerDto);
+  }
+}
