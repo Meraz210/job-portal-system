767078cf895a1681d0e43be0ae863aead90341a8
Meraz210 <merazahasan210@gmail.com>
2026-05-18 05:13:36 +0600

    implemented full job CRUD system

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
 
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
 
 import { JwtGuard } from '../auth/jwt.guard';
 import { RolesGuard } from '../auth/roles.guard';
@@ -31,5 +35,30 @@ export class JobsController {
   @Get()
   findAll() {
     return this.jobsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.jobsService.findOne(+id);
+  }
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(Role.EMPLOYER)
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateJobDto: UpdateJobDto,
+  ) {
+    return this.jobsService.update(
+      +id,
+      updateJobDto,
+    );
+  }
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(Role.EMPLOYER)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.jobsService.remove(+id);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 
 import { Job } from './entities/job.entity';
 import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
 
 @Injectable()
 export class JobsService {
@@ -20,5 +21,34 @@ export class JobsService {
 
   async findAll() {
     return this.jobRepository.find();
+  }
+
+  async findOne(id: number) {
+    return this.jobRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async update(
+    id: number,
+    updateJobDto: UpdateJobDto,
+  ) {
+    await this.jobRepository.update(
+      id,
+      updateJobDto,
+    );
+
+    return this.findOne(id);
+  }
+
+  async remove(id: number) {
+    const job = await this.findOne(id);
+
+    await this.jobRepository.delete(id);
+
+    return {
+      message: 'Job deleted successfully',
+      job,
+    };
   }
 }
